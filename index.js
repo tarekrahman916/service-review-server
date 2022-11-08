93b0ab70dd7a8b83ef8fe665bda34ae374470651
Md. Tarek Rahman <tarekrahman237@gmail.com>
2022-11-08 13:37:37 +0600

    added services in  mongodb

--- index.js
+++ index.js
@@ -4,23 +4,37 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const colors = require("colors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jdxzbss.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-// client.connect((err) => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
+
+async function run() {
+  try {
+    await client.connect();
+
+    const serviceCollection = client.db("photographer").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } catch (error) {
+    console.log(error.name.bgRed, error.massage);
+  }
+}
+
+run();
 
 app.get("/", (req, res) => {
   res.send("Server is running");
