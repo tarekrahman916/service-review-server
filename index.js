1094ddb20a45dc3b491ded866bc9a0d68d0b2398
Md. Tarek Rahman <tarekrahman237@gmail.com>
2022-11-08 13:07:14 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const colors = require("colors");
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jdxzbss.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
